056963506afd4359aff56b52b48d12a3bbc7a297
unknown <maksymiljanmajewski@gmail.com>
2025-12-21 14:22:29 +0100

    controllers prototypes

---
+++ MortgageComparer/Services/Interfaces/IReadService.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MortgageComparer.Services.Interfaces {
+    public interface IReadService<T, TKey> where T : BasicEntity {
+        List<T> GetAll();
+        T GetById(TKey id);
+    }
+
+    //To be removed
+    public class BasicEntity { }
+   
+}
