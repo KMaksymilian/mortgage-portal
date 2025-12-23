11eafd6c0e3425e64168758802e8529b89c60b13
unknown <maksymiljanmajewski@gmail.com>
2025-12-23 13:49:02 +0100

    before merge

--- MortgageComparer/Program.cs
+++ MortgageComparer/Program.cs
@@ -73,10 +73,7 @@ public class Program
 
         app.MapControllers();
 
-        using (var scope = app.Services.CreateScope()) {
-            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-            db.Database.Migrate();
-        }
+
 
         app.Run();
     }
